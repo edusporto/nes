6e33498739dc6b989e21f08cf1025f0abb783c75
Eduardo Sandalo Porto <eduardosandaloporto@gmail.com>
2022-11-17 19:26:21 -0300

    Fix double documentation

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,9 @@
-/// Module representing a 6502 CPU, used by the NES.
 pub mod cpu;
 
-/// Module for the Bus used by a 6502 CPU.
 pub mod bus;
 
-/// Module for the RAM used by the 6502 CPU.
 pub mod ram;
 
-/// Module for the Picture Processing Unit.
 pub mod ppu;
 
-/// Module for the game cartridge.
 pub mod cartridge;
